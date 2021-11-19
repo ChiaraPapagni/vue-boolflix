0f882a5da9d87afa87fe8648b24bb6c765d9e295
Chiara <chiaraiors@gmail.com>
2021-11-19 18:21:41 +0100

    fixed layout

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,17 +1,21 @@
 <template>
   <main id="site_main">
-    <div class="movies clearfix">
+    <div class="movies">
       <h2 v-if="movies.length > 0">Movie</h2>
-      <div class="movie" v-for="movie in movies" :key="movie.id">
-        <Movie :movie="movie" />
+      <div class="movie">
+        <div v-for="movie in movies" :key="movie.id">
+          <Movie :movie="movie" />
+        </div>
       </div>
     </div>
     <!-- /.movies -->
 
-    <div class="tv_show clearfix">
+    <div class="tv_show">
       <h2 v-if="tvShow.length > 0">TV Show</h2>
-      <div class="tv" v-for="tv in tvShow" :key="tv.id">
-        <TvShow :tv="tv" />
+      <div class="tv">
+        <div class="tv" v-for="tv in tvShow" :key="tv.id">
+          <TvShow :tv="tv" />
+        </div>
       </div>
     </div>
     <!-- /.tv_show -->
@@ -40,15 +44,17 @@ export default {
   width: 95%;
   margin: 4rem auto;
 
-  .movie,
-  .tv {
-    float: left;
+  h2 {
+    text-align: center;
+    margin: 4rem 0 2rem 0;
   }
 
-  .clearfix::after {
-    content: "";
-    display: table;
-    clear: both;
+  .movie,
+  .tv {
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    gap: 1rem;
   }
 }
 </style>
