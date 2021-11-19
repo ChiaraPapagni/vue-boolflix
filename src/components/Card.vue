6c669faf554cc0fd072bde1b8600b8f2ac30e167
Chiara <chiaraiors@gmail.com>
2021-11-19 11:52:18 +0100

    add overview

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,49 +6,60 @@
       :alt="item.name || item.title"
     />
     <img
-      width="185px"
       v-else
       src="../assets/img/not-available.png"
       alt="image not available"
+      width="185px"
     />
 
-    <!--  <h3>{{ item.name || item.title }}</h3>
-    <h3>{{ item.original_title || item.original_name }}</h3>
+    <div class="info">
+      <div class="title">
+        <p>Title: {{ item.name || item.title }}</p>
+        <p>Original title: {{ item.original_title || item.original_name }}</p>
+      </div>
+      <!-- /.title -->
 
-    <div class="language">
-      <span v-if="item.original_language == 'da'">
-        <flag iso="dm" :squared="false" />
-      </span>
-      <span v-else-if="item.original_language == 'en'">
-        <flag iso="gb" :squared="false" />
-      </span>
-      <span v-else-if="item.original_language == 'ja'">
-        <flag iso="jp" :squared="false" />
-      </span>
-      <span v-else-if="item.original_language == 'zh'">
-        <flag iso="cn" :squared="false" />
-      </span>
-      <span v-else>
-        <flag :iso="item.original_language" :squared="false" />
-      </span>
-    </div> -->
-    <!-- /.language -->
+      <div class="language">
+        <span v-if="item.original_language == 'da'">
+          <flag iso="dm" :squared="false" />
+        </span>
+        <span v-else-if="item.original_language == 'en'">
+          <flag iso="gb" :squared="false" />
+        </span>
+        <span v-else-if="item.original_language == 'ja'">
+          <flag iso="jp" :squared="false" />
+        </span>
+        <span v-else-if="item.original_language == 'zh'">
+          <flag iso="cn" :squared="false" />
+        </span>
+        <span v-else>
+          <flag :iso="item.original_language" :squared="false" />
+        </span>
+      </div>
+      <!-- /.language -->
 
-    <!--     <div class="rating">
-      <span
-        v-for="(vote, i) in Math.ceil(item.vote_average / 2)"
-        :key="'fas' + i"
-      >
-        <font-awesome-icon :icon="['fas', 'star']" />
-      </span>
-      <span
-        v-for="(vote, i) in 5 - Math.ceil(item.vote_average / 2)"
-        :key="'far' + i"
-      >
-        <font-awesome-icon :icon="['far', 'star']" />
-      </span>
-    </div> -->
-    <!-- /.rating -->
+      <div class="rating">
+        <span
+          v-for="(vote, i) in Math.ceil(item.vote_average / 2)"
+          :key="'fas' + i"
+        >
+          <font-awesome-icon :icon="['fas', 'star']" />
+        </span>
+        <span
+          v-for="(vote, i) in 5 - Math.ceil(item.vote_average / 2)"
+          :key="'far' + i"
+        >
+          <font-awesome-icon :icon="['far', 'star']" />
+        </span>
+      </div>
+      <!-- /.rating -->
+
+      <div class="overview">
+        <p>Overview: {{ item.overview }}</p>
+      </div>
+      <!-- /.overview -->
+    </div>
+    <!-- /.info -->
   </div>
   <!-- /.card -->
 </template>
